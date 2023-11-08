81d44aba6a9016a2a822a21e1cffdac03b28aaa8
임준영 <imjun-yeong@imjun-yeong-ui-MacBookPro.local>
2023-11-09 01:09:38 +0900

    init

---
+++ src/test/java/com/goalmokgil/gmk/GmkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goalmokgil.gmk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
