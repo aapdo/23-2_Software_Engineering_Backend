e175d4dfefba240aa25fb724841eb612c23e4963
wooseo1 <gwoo0510@naver.com>
2023-11-24 14:27:15 +0900

    delete mongo

--- src/main/java/com/goalmokgil/gmk/course/repository/CourseDataMapping.java
+++
@@ -1,8 +0,0 @@
-package com.goalmokgil.gmk.course.repository;
-
-import io.hypersistence.utils.hibernate.type.json.JsonType;
-
-public interface CourseDataMapping {
-    Long
-    String getCourseData();
-}
