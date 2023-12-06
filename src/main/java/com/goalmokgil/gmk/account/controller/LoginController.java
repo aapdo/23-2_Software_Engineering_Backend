d72142f7e45a685338c576130ea31ed0d6774d2e
aapdo <jade04342@gmail.com>
2023-12-06 20:38:23 +0900

    add loginToken

--- src/main/java/com/goalmokgil/gmk/account/controller/LoginController.java
+++ src/main/java/com/goalmokgil/gmk/account/controller/LoginController.java
@@ -3,8 +3,13 @@ package com.goalmokgil.gmk.account.controller;
 import com.goalmokgil.gmk.account.dto.JwtLoginDto;
 import com.goalmokgil.gmk.account.dto.req.ReqLoginDto;
 import com.goalmokgil.gmk.account.service.LoginService;
+import com.goalmokgil.gmk.account.service.TokenService;
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +21,12 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public JwtLoginDto login(@RequestBody ReqLoginDto reqLoginDto) {
+
+    public ResponseEntity<JwtLoginDto> login(@RequestBody ReqLoginDto reqLoginDto) {
         String memberId = reqLoginDto.getLoginId();
         String password = reqLoginDto.getPassword();
+
+
 
         JwtLoginDto jwtLoginDto = JwtLoginDto.builder()
                 .grantType(loginService.login(memberId, password).getGrantType())
@@ -26,6 +34,17 @@ public class LoginController {
                 .loginId(reqLoginDto.getLoginId())
                 .build();
 
-        return jwtLoginDto;
+        HttpHeaders header = new HttpHeaders();
+        Cookie loginCookie = new Cookie("loginToken", jwtLoginDto.getAccessToken());
+
+        loginCookie.setPath("/");
+        // 30일 간 쿠키 유지.
+        loginCookie.setMaxAge(60 * 60 * 24 * 30);
+
+        header.add("Set-Cookie", loginCookie.toString());
+
+        return ResponseEntity.ok()
+                .headers(header)
+                .body(jwtLoginDto);
     }
 }
